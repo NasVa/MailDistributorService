8795cb13d19cd8b05839f9e6e58a676f85e0291d
NasVa <aska.vakulchik@mail.ru>
2022-07-06 01:56:18 +0300

    added classes

---
+++ mailDistributorService/Models/API/IAPI.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CsvHelper;
+
+namespace mailDistributorService.Models.API
+{
+    interface IAPI
+    {
+        
+    }
+}
---
+++ mailDistributorService/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mailDistributorService.Models
+{
+    public class Task
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime periodDate { get; set; }
+        public DateTime lastSend { get; set; }
+
+    }
+}
